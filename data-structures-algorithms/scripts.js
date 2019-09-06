cd0af1433a64c0a8cdd254fe988e9518f66c3565
Bets <elizabeth.lidstrom@gmail.com>
2019-09-05 20:12:32 -0400

    most recurring character

--- data-structures-algorithms/scripts.js
+++ data-structures-algorithms/scripts.js
@@ -83,4 +83,60 @@ document.getElementById('vowel-solution').innerText = vowelsCounter2('you got th
 
 
 
+// ========== 3. The Most Recurring Character ========== //
+
+// For ... in Iteration - somewhat more optimal ~30%
+const maxRecurringChar = (text) => {
+	let charMap = {};
+	let maxCharValue = 0;
+	let maxChar = '';
+
+	for (let char of text) {
+		if (charMap.hasOwnProperty(char)) {
+			charMap[char]++
+		} else {
+			charMap[char] = 1;
+		}
+	}	
+
+	for (let char in charMap) {
+		if (charMap[char] > maxCharValue) {
+			maxCharValue = charMap[char]
+			maxChar = char
+		}
+	}
+
+	return maxChar;
+}
+
+// From Object to Array ...
+const maxRecurringChar2 = (text) => {
+	let charMap = {};
+	let charArray = [];
+	let valuesArray = [];
+	let maxCharValue = 0;
+
+	for (let char of text) {
+		if (charMap.hasOwnProperty(char)) {
+			charMap[char]++
+		} else {
+			charMap[char] = 1
+		}
+	}
+
+	charArray = Object.keys(charMap)
+	valuesArray = Object.values(charMap)
+	maxCharValue = Math.max(...valuesArray)
+
+	return charArray[valuesArray.indexOf(maxCharValue)];
+}
+
+document.getElementById('max-char-solution').innerText = maxRecurringChar2('Tierney');
+
+
+
+
+
+
+
 
