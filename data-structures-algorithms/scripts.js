ee73d728fb4093c92e2d6fdf3d72cc6bb2d86170
Bets <elizabeth.lidstrom@gmail.com>
2019-09-14 12:59:49 -0400

    anagrams

--- data-structures-algorithms/scripts.js
+++ data-structures-algorithms/scripts.js
@@ -303,6 +303,53 @@ document.getElementById('search-replace-solution').innerText = searchReplace2('I
 
 
 
+// ========== 9. Anagrams ========== //
+
+// Direct Comparison
+const isAnagram = (stringA, stringB) => {
+	
+	const sanitizeString = (str) => {
+		return str.toLowerCase().replace(/[^a-z\d]/g, '').split('').sort().join('');
+	}
+	return sanitizeString(stringA) == sanitizeString(stringB); 
+
+}
+
+// Character Map Comparison // Most Optimal
+const isAnagram2 = (stringA, stringB) => {
+	
+	const createCharMap = (text) => {
+		let charMap = {};
+
+		for (let char of text) {
+			if (charMap.hasOwnProperty(char)) {
+				charMap[char]++
+			} else {
+				charMap[char] = 1;
+			}
+		}
+
+		return charMap;
+	}
+
+	if (stringA.length === stringB.length) {
+		let stringAMap = createCharMap(stringA);
+		let stringBMap = createCharMap(stringB);
+		for (let char in stringAMap) {
+			if (stringAMap[char] !== stringBMap[char]) {
+				return false;
+			}
+		}
+		return true;
+	} else {
+		return false;
+	}
+
+}
+
+document.getElementById('anagrams-solution').innerText = isAnagram2('silent', 'listen');
+
+
 
 
 
